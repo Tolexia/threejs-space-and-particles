c2322f7685561ee93df13e4eee58d03f033a9fbf
Axel C <69471505+Tolexia@users.noreply.github.com>
2023-09-22 14:01:54 +0200

    Init vite structure

---
+++ main.js
@@ -0,0 +1,11 @@
+import { Scene, WebGLRenderer } from 'three'
+import './style.css'
+
+const scene = new Scene;
+
+const renderer = new WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio,2));
+
+document.body.appendChild(renderer.domElement);
